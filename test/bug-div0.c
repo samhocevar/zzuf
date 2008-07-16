63b3a8e8a0352b85833695ba913fb0d81196d67f
Sam Hocevar <sam@hocevar.net>
2008-07-16 17:23:37 +0000

    * Add a bunch of unit tests to check for zzuf basic capabilities: SIGSEGV     and SIGFPE detection, memory exhaustion detection.

---
+++ test/bug-div0.c
@@ -0,0 +1,31 @@
+/*
+ *  bug-div0 - program dividing by zero when fuzzed
+ *  Copyright (c) 2008 Sam Hocevar <sam@zoy.org>
+ *                All Rights Reserved
+ *
+ *  $Id$
+ *
+ *  This program is free software. It comes without any warranty, to
+ *  the extent permitted by applicable law. You can redistribute it
+ *  and/or modify it under the terms of the Do What The Fuck You Want
+ *  To Public License, Version 2, as published by Sam Hocevar. See
+ *  http://sam.zoy.org/wtfpl/COPYING for more details.
+ */
+
+#include "config.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int buf[1];
+
+int main(void)
+{
+    int ch;
+
+    while((ch = getc(stdin)) != EOF)
+        buf[0] = 1 / !ch;
+
+    return EXIT_SUCCESS;
+}
+
