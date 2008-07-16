6c23cd7e29975cfc9b7762aa7cfc2e6650bb7c1e
Sam Hocevar <sam@hocevar.net>
2008-07-16 21:41:27 +0000

    * Another fucked commit... synchronise config.h and lib-stream.c.

--- src/lib-stream.c
+++ src/lib-stream.c
@@ -144,7 +144,7 @@ int            (*ORIG(__filbuf))  (FILE *fp);
 /* Our function wrappers */
 #if defined HAVE_REFILL_STDIO /* Fuzz fp if we have __srefill() */
 #   define FOPEN_FUZZ() \
-    _zz_fuzz(fd, ret->FP_PTR, ret->FP_CNT)
+    _zz_fuzz(fd, ret->FILE_PTR, ret->FILE_CNT)
 #else
 #   define FOPEN_FUZZ()
 #endif
@@ -794,10 +794,10 @@ int NEW(__filbuf)(FILE *fp)
     if(ret != EOF)
     {
         if(newpos != -1)
-            _zz_setpos(fd, newpos - fp->FP_CNT - 1);
-        _zz_fuzz(fd, fp->FP_PTR - 1, fp->FP_CNT + 1);
-        ret = (uint8_t)fp->FP_PTR[-1];
-        _zz_addpos(fd, fp->FP_CNT + 1);
+            _zz_setpos(fd, newpos - fp->FILE_CNT - 1);
+        _zz_fuzz(fd, fp->FILE_PTR - 1, fp->FILE_CNT + 1);
+        ret = (uint8_t)fp->FILE_PTR[-1];
+        _zz_addpos(fd, fp->FILE_CNT + 1);
     }
 
     if(!_zz_islocked(fd))
