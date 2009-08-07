5d2b1b7682eb3ea9e26fbd6bdfe598bb8abf6fb7
Sam Hocevar <sam@hocevar.net>
2009-08-07 00:09:03 +0000

    Old libtool versions don't define LT_OBJDIR. Adding an override for them.

--- src/zzuf.c
+++ src/zzuf.c
@@ -81,6 +81,11 @@
 #   define SIGKILL 9
 #endif
 
+/* Handle old libtool versions */
+#if !defined LT_OBJDIR
+#   define LT_OBJDIR ".libs"
+#endif
+
 #if defined RLIMIT_AS
 #   define ZZUF_RLIMIT_MEM RLIMIT_AS
 #elif defined RLIMIT_VMEM
