0f775dadc8604a00f7e37f55e74b78d4a2541cbd
Sam Hocevar <sam@hocevar.net>
2009-11-20 12:06:17 +0000

    Add zznop, a small test program to help debug the Win32 port. Also add check-build from libcaca to test that msvc/config.h is up to date.

---
+++ test/zznop.c
@@ -0,0 +1,37 @@
+/*
+ *  zznop - almost empty program that does almost nothing
+ *  Copyright (c) 2009 Sam Hocevar <sam@hocevar.net>
+ *                All Rights Reserved
+ *
+ *  $Id$
+ *
+ *  This program is free software. It comes without any warranty, to
+ *  the extent permitted by applicable law. You can redistribute it
+ *  and/or modify it under the terms of the Do What The Fuck You Want
+ *  To Public License, Version 2, as published by Sam Hocevar. See
+ *  http://sam.zoy.org/wtfpl/COPYING for more details.
+ */
+
+#include "config.h"
+
+#include <stdio.h>
+
+#if defined HAVE_WINDOWS_H
+#   include <windows.h>
+#endif
+
+int main(void)
+{
+#if defined HAVE_WINDOWS_H
+    AllocConsole();
+
+    fprintf(stderr, "About to call LoadLibraryA()\n");
+    LoadLibraryA("whatever");
+    fprintf(stderr, "Finished calling LoadLibraryA()\n");
+
+    getchar();
+#endif
+
+    return EXIT_SUCCESS;
+}
+
