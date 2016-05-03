012da78b046cb18437abd034d26678d5d0223e72
Sam Hocevar <sam@hocevar.net>
2016-05-03 08:19:12 +0200

    Prepare 0.15 release.

--- src/libzzuf/lib-stream.c
+++ src/libzzuf/lib-stream.c
@@ -1,7 +1,7 @@
 /*
  *  zzuf - general purpose fuzzer
  *
- *  Copyright © 2002—2015 Sam Hocevar <sam@hocevar.net>
+ *  Copyright © 2002—2016 Sam Hocevar <sam@hocevar.net>
  *
  *  This program is free software. It comes without any warranty, to
  *  the extent permitted by applicable law. You can redistribute it
